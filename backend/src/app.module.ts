4def616fc80c3081864b19d34e94a55ab1364146
pabloacisera <pabloacisera2021@gmail.com>
2026-08-02 00:36:52 -0300

    feat: wire all modules in AppModule, enable strict TypeScript

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { PrismaModule } from './prisma';
+import { CacheModule } from './cache';
+import { RealtimeModule } from './realtime';
+import { TelemetryModule } from './telemetry';
+import { CommandModule } from './command/command.module';
+import { MotorsModule } from './motors';
+import { AlertsModule } from './alerts';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    PrismaModule,
+    CacheModule,
+    RealtimeModule,
+    CommandModule,
+    TelemetryModule,
+    MotorsModule,
+    AlertsModule,
+  ],
+})
+export class AppModule {}
